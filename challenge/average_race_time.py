ade01819e9b8703eda83b8d54786c4f868c10cf3
Suryakanth Gnanakadalarasan <39038522+503217@users.noreply.github.com>
2023-01-09 14:59:25 +0000

    Pending changes exported from your codespace

--- challenge/average_race_time.py
+++ challenge/average_race_time.py
@@ -4,16 +4,21 @@
 import re
 import datetime
 
+
 def get_data():
     """Return content from the 10k_racetimes.txt file"""
     with open('10k_racetimes.txt', 'rt') as file:
-        content = file.read()
+        content = file.readlines()
     return content
+
 
 def get_rhines_times():
     """Return a list of Jennifer Rhines' race times"""
     races = get_data()
-    pass
+    timings = [runner.split()[0]
+               for runner in races if 'jennifer' in runner.lower()]
+    return timings
+
 
 def get_average():
     """Return Jennifer Rhines' average race time in the format:
@@ -22,4 +27,18 @@ def get_average():
        s corresponds to a seconds digit
        M corresponds to a milliseconds digit (no rounding, just the single digit)"""
     racetimes = get_rhines_times()
-    pass
+    total = datetime.timedelta()
+    for timing in racetimes:
+        try:
+            mins, secs, msecs = re.split(r'[:.]', timing)
+        except:
+            mins, secs = re.split(r'[:.]', timing)
+            msecs = 0
+        total += datetime.timedelta(minutes=int(mins),
+                                    seconds=int(secs), milliseconds=int(msecs))
+
+    return f'{total/len(racetimes)}'[2:-5]
+    # total+=datetime.timedelta(minutes=)
+
+
+print(get_average())
